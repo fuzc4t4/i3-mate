cae95926c85779cce90dfa72a650babc772a541f
fuzc4t4 <fuzc4t4@loremipsum.dolor>
2023-06-06 00:10:57 -0600

    Fixed 2 bugs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,7 +4,7 @@ killall -q polybar
 
 if type "xrandr"; then
     PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
-    MONITOR=$PRIMARY polybar --reload &
+    MONITOR=$PRIMARY polybar useful --reload &
     sleep 1
     for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1); do
         MONITOR=$m polybar --reload useful &
